6599442361097146f329651e054949a319c57b2c
Paul Niemczyk <mrpndev@gmail.com>
2021-01-26 09:43:58 -0500

    Created functional component and mounted external component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ function App() { // root app component
   const name = "Paul" // JS above return injected into JSX with { }
                       // can also use JS if outside elements
   return (
-    // <div className="App">
-    //   <h1>Welcome to React, {name}</h1>
-    //   <h2>Single Page Application</h2>
-    // </div>
-    <div className="container">
-      <Header />
+    <div className="App">
+    <h1>Welcome to React, {name}</h1>
+    <h2>Single Page Application</h2>
+    <Header /> { /* mounting of import component */ }
     </div>
-
   );
 }
-
 // module.exports = App;
 export default App;
