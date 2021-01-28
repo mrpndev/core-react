483b1f240cedab7ea5f0565a67e0ba5b7b287940
Paul Niemczyk <mrpndev@gmail.com>
2021-01-28 12:30:06 -0500

    Clean develop branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import Header from './components/Header'
 
 function App() { // root app component
-  const name = "Paul" // JS above return injected into JSX with { }
-                      // can also use JS if outside elements
   return (
     <div className="App">
-    <h1>Welcome to React, {name}</h1>
-    <h2>Single Page Application</h2>
-    <Header /> { /* mounting of import component */ }
     </div>
   );
 }
